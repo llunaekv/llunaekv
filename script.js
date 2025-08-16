0b0fb8083c299bc70ef6350532e5ef9ec93f9311
lucia <158306747+llunaekv@users.noreply.github.com>
2025-08-16 19:02:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,41 @@ function analizarEmocion() {
     return res.json();
   })
   .then(data => {
-    // Cambia 'respuesta' por el campo correcto de tu respuesta Flask si es necesario
     if (data.respuesta) {
       respuesta.innerHTML = data.respuesta;
+    } else {
+      respuesta.innerHTML = "No se recibió respuesta de la IA";
+    }
+  })
+  .catch(error => {
+    respuesta.innerHTML = "Hubo un error al conectar con el servidor 😢<br>" + error;
+    console.error(error);
+  });
+}
+
+function preguntarIA() {
+  const mensaje = document.getElementById("userInput").value;
+  const respuesta = document.getElementById("respuestaIA");
+
+  respuesta.innerHTML = "Pensando...";
+
+  fetch('https://llunaekv.pythonanywhere.com/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message: mensaje })
+  })
+  .then(res => {
+    if (!res.ok) {
+      respuesta.innerHTML = "Error de servidor: " + res.status;
+      throw new Error("Error de servidor: " + res.status);
+    }
+    return res.json();
+  })
+  .then(data => {
+    if (data.reply) {
+      respuesta.innerHTML = data.reply;
     } else {
       respuesta.innerHTML = "No se recibió respuesta de la IA";
     }
